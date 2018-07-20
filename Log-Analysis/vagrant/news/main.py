2b2daa3404d0352134a001836d101f395c4a94f7
Dawn <xuedonglin1994@gmail.com>
2018-07-20 17:33:22 +0800

    fix: delete flask

--- Log-Analysis/vagrant/news/main.py
+++ Log-Analysis/vagrant/news/main.py
@@ -1,7 +1,5 @@
 import psycopg2
-from flask import Flask
 
-app = Flask(__name__)
 
 DBNAME = "news"
 
@@ -55,56 +53,3 @@ def bad_request():
     result = c.fetchone()
     db.close()
     return result
-
-
-HTML_WRAP = '''\
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>DB Forum</title>
-    <style>
-      h1, form { text-align: center; }
-      textarea { width: 400px; height: 100px; }
-      div.post { border: 1px solid #999;
-                 padding: 10px 10px;
-                 margin: 10px 20%%; }
-      hr.postbound { width: 50%%; }
-      em.date { color: #999 }
-    </style>
-  </head>
-  <body>
-    <h1>1. What are the most popular three articles of all time? </h1>
-    <ul>
-        %s
-    </ul>
-
-    <h1>2. Who are the most popular article authors of all time? </h1>
-    <ul>
-        %s
-    </ul>
-
-    <h1>3. On which days did more than 1 percent of requests lead to errors?</h1>
-    <ul>
-        %s
-    </ul>
-  </body>
-</html>
-'''
-
-ENTRY = """<li>%s ---- %s %s </li>"""
-
-
-@app.route('/', methods=['GET'])
-def index():
-    article_list = "".join(ENTRY % (title, str(views), 'views')
-                           for title, views in popular_article())
-    author_list = "".join(ENTRY % (author, str(views), 'views')
-                          for author, views in popular_author())
-    error_date = "".join(ENTRY % (bad_request()[0], bad_request()[1], 'error'))
-
-    html = HTML_WRAP % (article_list, author_list, error_date)
-    return html
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
