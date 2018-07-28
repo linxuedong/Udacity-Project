3f92ce647068ab336d9eb825a2933b63eed4cd25
Dawn <xuedonglin1994@gmail.com>
2018-07-28 16:01:21 +0800

    style: use triple quotation mark notation

--- Log-Analysis/vagrant/news/main.py
+++ Log-Analysis/vagrant/news/main.py
@@ -12,12 +12,13 @@ def popular_article():
     """
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("SELECT title, count(path) AS view \
-                FROM articles JOIN log \
-                ON path = CONCAT('/article/', slug) \
-                GROUP BY title \
-                ORDER BY view DESC \
-                LIMIT 3;")
+    c.execute("""
+        SELECT title, count(path) AS view
+        FROM articles JOIN log
+        ON path = CONCAT('/article/', slug)
+        GROUP BY title
+        ORDER BY view DESC
+        LIMIT 3;""")
     result = c.fetchall()
     db.close()
     return result
@@ -29,15 +30,16 @@ def popular_author():
     """
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("SELECT name, view \
-                FROM authors JOIN ( \
-                    SELECT author,  count(author) AS view \
-                        FROM articles JOIN log \
-                        ON path = CONCAT('/article/', slug)\
-                        GROUP BY author \
-                        ORDER BY view DESC \
-                    ) AS popular_author \
-                ON authors.id = popular_author.author;")
+    c.execute("""
+        SELECT name, view
+        FROM authors JOIN (
+            SELECT author,  count(author) AS view
+                FROM articles JOIN log
+                ON path = CONCAT('/article/', slug)
+                GROUP BY author
+                ORDER BY view DESC
+            ) AS popular_author
+        ON authors.id = popular_author.author;""")
     result = c.fetchall()
     db.close()
     return result
@@ -49,20 +51,21 @@ def bad_request():
     """
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("SELECT * \
-        FROM ( \
-            SELECT date(time), \
-            to_char(100.0*bad.b_view::numeric/count(*)::numeric, '999D99%')\
-             as percent \
-            FROM (SELECT date(time) AS b_date, count(*) AS b_view \
-            FROM log \
-            WHERE status NOT LIKE '200%' \
-            GROUP BY date(time)) AS bad JOIN log \
-            ON date(time) = bad.b_date \
-            GROUP BY date(time), bad.b_view \
-        ) AS result \
-        WHERE to_number(percent, '999D99%') > 1 \
-        ;")
+    c.execute("""
+        SELECT *
+        FROM (
+            SELECT date(time),
+            to_char(100.0*bad.b_view::numeric/count(*)::numeric, '999D99%')
+             as percent
+            FROM (SELECT date(time) AS b_date, count(*) AS b_view
+            FROM log
+            WHERE status NOT LIKE '200%'
+            GROUP BY date(time)) AS bad JOIN log
+            ON date(time) = bad.b_date
+            GROUP BY date(time), bad.b_view
+        ) AS result
+        WHERE to_number(percent, '999D99%') > 1
+        ;""")
     result = c.fetchone()
     db.close()
     return result
